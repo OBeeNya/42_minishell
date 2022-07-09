2daf8a2494a889250c179872bec8ef5d4234339b
Benjamin AUBIGNAT <baubigna@e1r3p16.clusters.42paris.fr>
2022-07-09 17:44:17 +0200

    exit long int

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,13 +6,38 @@
 /*   By: baubigna <baubigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 17:53:41 by baubigna          #+#    #+#             */
-/*   Updated: 2022/07/09 17:33:41 by baubigna         ###   ########.fr       */
+/*   Updated: 2022/07/09 17:42:50 by baubigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 extern t_bash	g_bash;
+
+unsigned long long int	ft_long_atoi(const char *str)
+{
+	int						i;
+	int						neg;
+	unsigned long long int	nb;
+
+	i = 0;
+	neg = 1;
+	nb = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nb);
+}
 
 int	ft_is_str_num(char *s)
 {
@@ -25,6 +50,8 @@ int	ft_is_str_num(char *s)
 			return (0);
 		i++;
 	}
+	if (ft_long_atoi(s) > 9223372036854775807)
+		return (0);
 	return (1);
 }
 
