ac7f523aec803a6d6976b82834ce3d2a602fd82c
Benjamin AUBIGNAT <baubigna@e1r3p16.clusters.42paris.fr>
2022-07-09 17:35:11 +0200

    exit too many arguments

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,11 +6,13 @@
 /*   By: baubigna <baubigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 17:53:41 by baubigna          #+#    #+#             */
-/*   Updated: 2022/07/09 13:01:54 by baubigna         ###   ########.fr       */
+/*   Updated: 2022/07/09 17:33:41 by baubigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+extern t_bash	g_bash;
 
 int	ft_is_str_num(char *s)
 {
@@ -30,9 +32,15 @@ void	ft_exit(t_pipe *pipe)
 {
 	if (pipe->args)
 	{
+		if (pipe->args[1])
+		{
+			ft_putstr_fd("minishell: exit\ntoo many arguments\n", 2);
+			g_bash.err = 1;
+			return ;
+		}
 		if (!ft_is_str_num(pipe->args[0]))
 		{
-			ft_putstr_fd("bash: exit: ", 2);
+			ft_putstr_fd("minishell: exit: ", 2);
 			ft_putstr_fd(pipe->args[0], 2);
 			ft_putstr_fd(": numeric argument required\n", 2);
 			exit(2);
