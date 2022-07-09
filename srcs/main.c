8f0c0e063020b30240f86baa1b4164258786cce5
Benjamin AUBIGNAT <baubigna@e1r3p16.clusters.42paris.fr>
2022-07-09 17:25:51 +0200

    ctrl C => bash.err = 130 / via global var

--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,16 @@
 /*   By: baubigna <baubigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 11:26:44 by baubigna          #+#    #+#             */
-/*   Updated: 2022/07/08 13:48:18 by baubigna         ###   ########.fr       */
+/*   Updated: 2022/07/09 17:23:34 by baubigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+t_bash	g_bash;
+
 int	main(int ac, char **av, char **envp)
 {
-	t_bash	bash;
 	bool	first;
 	bool	last;
 
@@ -26,8 +27,8 @@ int	main(int ac, char **av, char **envp)
 		return (1);
 	}
 	(void) av;
-	bash.err = 0;
-	bash.echo = 0;
-	ft_prompt(&bash, envp, first, last);
+	g_bash.err = 0;
+	g_bash.echo = 0;
+	ft_prompt(&g_bash, envp, first, last);
 	return (0);
 }
--- srcs/signals.c
+++ srcs/signals.c
@@ -6,11 +6,13 @@
 /*   By: baubigna <baubigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 15:36:01 by baubigna          #+#    #+#             */
-/*   Updated: 2022/07/09 13:25:51 by baubigna         ###   ########.fr       */
+/*   Updated: 2022/07/09 17:24:07 by baubigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+extern t_bash	g_bash;
 
 void	ctrl_handler(int signum)
 {
@@ -18,6 +20,7 @@ void	ctrl_handler(int signum)
 	{
 		rl_replace_line("", 0);
 		ft_putstr_fd("\nminishell ~ ", 1);
+		g_bash.err = 130;
 	}
 	else if (signum == SIGSEGV)
 	{
